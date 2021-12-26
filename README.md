a348a40c9d75541ec9a49061d30a3516c7d64383
GCS Council <95979300+GCSCOUNCIL@users.noreply.github.com>
2021-12-26 22:37:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+#### Discription
+GCS_crawler can extract the following data while crawling:
+
+- URLs (in-scope & out-of-scope)
+- URLs with parameters (`example.com/gallery.php?id=2`)
+- Intel (emails, social media accounts, amazon buckets etc.)
+- Files (pdf, png, xml etc.)
+- Secret keys (auth/API keys & hashes)
+- JavaScript files & Endpoints present in them
+- Strings matching custom regex pattern
+- Subdomains & DNS related data
+
+
+
+#### Flexible
+Control timeout, delay, add seeds, exclude URLs matching a regex pattern and other cool stuff.
+
+#### Discription
+GCS_crawler smart thread management & refined logic gives you top notch performance.
+
+Still, crawling can be resource intensive but Photon has some tricks up it's sleeves. You can fetch URLs archived by [archive.org](https://archive.org/) to be used as seeds by using `--wayback` option.
+
+
---
+++ plugins/find_subdomains.py
@@ -0,0 +1,29 @@
+
+print('''
+
+░██████╗░░█████╗░░██████╗░█████╗░░█████╗░██╗░░░██╗███╗░░██╗░█████╗░██╗██╗░░░░░
+██╔════╝░██╔══██╗██╔════╝██╔══██╗██╔══██╗██║░░░██║████╗░██║██╔══██╗██║██║░░░░░
+██║░░██╗░██║░░╚═╝╚█████╗░██║░░╚═╝██║░░██║██║░░░██║██╔██╗██║██║░░╚═╝██║██║░░░░░
+██║░░╚██╗██║░░██╗░╚═══██╗██║░░██╗██║░░██║██║░░░██║██║╚████║██║░░██╗██║██║░░░░░
+╚██████╔╝╚█████╔╝██████╔╝╚█████╔╝╚█████╔╝╚██████╔╝██║░╚███║╚█████╔╝██║███████╗
+░╚═════╝░░╚════╝░╚═════╝░░╚════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝░╚════╝░╚═╝╚══════╝
+-----------------------------------------------------------------------------
+Developed by: GCSCOUNCIL
+Company: GCSCOUNCIL
+''')
+
+
+
+from re import findall
+
+from requests import get
+
+
+def find_subdomains(domain):
+    """Find subdomains according to the TLD."""
+    result = set()
+    response = get('https://findsubdomains.com/subdomains-of/' + domain).text
+    matches = findall(r'(?s)<div class="domains js-domain-name">(.*?)</div>', response)
+    for match in matches:
+        result.add(match.replace(' ', '').replace('\n', ''))
+    return list(result)
